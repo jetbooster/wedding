643db42138c0956942742335cfdb5e0d84dd7eba
jetbooster <jarvisam@gmail.com>
2026-02-15 00:54:04 +0000

    protect nat

--- src/components/details/ContactUs.tsx
+++ src/components/details/ContactUs.tsx
@@ -15,12 +15,14 @@ const ContactUs: FC = () => {
         Claudine: <Link href="tel:+447948347550">+44 7948 347 550</Link>
       </Typography>
       <Typography>
-        <Trans
-          i18nKey="details.contactUs.nat"
-          components={{
-            1: <Link href="tel:+447736350670" />,
-          }}
-        ></Trans>
+        {new Date() > new Date("2026-08-21T00:00:00.000Z") && (
+          <Trans
+            i18nKey="details.contactUs.nat"
+            components={{
+              1: <Link href="tel:+447736350670" />,
+            }}
+          />
+        )}
       </Typography>
     </Grid>
   );
