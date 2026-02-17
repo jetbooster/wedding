cafcf2e533831d5d88299d631f9a8b85d4305283
jetbooster <jetbooster>
2026-02-17 17:57:48 +0000

    fix: translation erros

--- src/i18n/fr/translation.ts
+++ src/i18n/fr/translation.ts
@@ -1,49 +1,49 @@
 export default {
   attendingBlock: {
     attending: {
-      label: "Serez-vous des nôtres?",
+      label: "Serez-vous des nôtres ?",
       no: "Décline avec regret",
       yes: "Accepte avec plaisir",
     },
-    name: "Votre nom: (veuillez corriger des erreurs d'orthograpie)",
+    name: "Votre nom : (veuillez corriger des erreurs d'orthographe)",
   },
   childAttendingBlock: {
-    childName: "Nom d'enfant {{count}}:",
-    bringOwnFood: "Nous apporterons leur propre repas",
-    highchair: "Une chaise haute sera nćessaire",
-    description: "Merci de prevoir un menu d'enfant (Entrée: Aiguillettes de poulets, sauce BBQ, Plat: Linguine à la tomate Dessert: Cookie au pépites de chocolat)",
+    childName: "Nom d'enfant {{count}} :",
+    bringOwnFood: "Nous apporterons son propre repas",
+    highchair: "Une chaise haute sera nécessaire",
+    description: "Merci de prévoir un menu d'enfant (Entrée : Aiguillettes de poulet, sauce BBQ ; Plat : Linguine à la tomate ; Dessert : Cookie aux pépites de chocolat)",
   },
   common: {
     and: "&",
     button: {
       submit: "Envoyer {{extra}}",
-      back: "Retour à l'acceuil",
+      back: "Retour à l'accueil",
       tryAgain: "Réessayer"
     },
   },
   details: {
     accomodation: {
       description:
-        "Un bloc de chambres à tarif preéférentiel est dissponbible à Rookery Hall pour la nuit du mariage. Veuillez mentionner nos noms lors de votre réservation par téléphone.",
+        "Un bloc de chambres à tarif préférentiel est disponible à Rookery Hall pour la nuit du mariage. Veuillez mentionner nos noms lors de votre réservation par téléphone.",
       nearby: "hôtels à proximité",
       other: "Si vous souhaitez explorer d'autres options, voici quelques ",
       title: "Hébergement",
     },
     contactUs: {
       description:
-        "Si vous avez des questions concernant le voyage, le lieu de réception ou toute autre chose, n'hesitez pas à nous contactez.",
-      nat: "Le jour du mariage : Veuille contacter notre demoiselle d'honneur, Natalie au  <1>+44 7736 350 670</1>.",
-      title: "Contactez-nous",
+        "Si vous avez des questions concernant le voyage, le lieu de réception ou toute autre chose, n'hésitez pas à nous contacter.",
+      nat: "Le jour du mariage : Veuillez contacter notre demoiselle d'honneur, Natalie, au <1>+44 7736 350 670</1>.",
+      title: "Contacter",
     },
     dateAndTime: {
       ceremony: "Cérémonie à midi",
-      longDate: "Samedi, 22 août 2026",
+      longDate: "Samedi 22 août 2026",
       seated: "Merci de prendre place à 11h45",
       title: "Date et Heure",
     },
     dressCode: {
       description:
-        "Nous esperons organiser une cérémonie en plein air (si la météo le permet !). Comme cela implique de marcher sur l'herbe, nous vous conseillons de choisir vos chaussures en conséquence.",
+        "Nous espérons organiser une cérémonie en plein air (si la météo le permet !). Comme cela implique de marcher sur l'herbe, nous vous conseillons de choisir vos chaussures en conséquence.",
       dressCode: "Tenue de soirée exigée",
       title: "Tenue de fête",
     },
@@ -53,7 +53,7 @@ export default {
       international:
         "Invités internationaux : IBAN : GB56SRLG60837162999637 | Code SWIFT/BIC : SRLGGB2L",
       national:
-        "Virement bancaire: Nom de compte : Claudine Richardson | Code guichet (Sort Code): 60-83-71 | Numéro de compte : 62999637",
+        "Virement bancaire : Nom de compte : Claudine Richardson | Code guichet (Sort Code) : 60-83-71 | Numéro de compte : 62999637",
       paypal: "Participer via PayPal",
       title: "Cadeaux",
     },
@@ -65,14 +65,14 @@ export default {
       cutting: "Découpe du gâteau",
       dancing: "Soirée dansante",
       drinks: "Vin d'honneur, canapés et photos",
-      seated: "Instalation des invités",
+      seated: "Installation des invités",
       speeches: "Discours et toasts",
       title: "Le déroulement de la journée",
     },
     transport: {
       airport:
-        "<1>L'aéoroport le plus proche est <2>l'aéroport de Manchester</2>, à environ 40 minutes de route. Vous trouverez l'itinéraire Google Maps <3>ici</3>.</1>",
-      parking: "Il y a de nombreuses places de parking disponibles gratuitement sure place.",
+        "<1>L'aéroport le plus proche est <2>l'aéroport de Manchester</2>, à environ 40 minutes de route. Vous trouverez l'itinéraire Google Maps <3>ici</3>.</1>",
+      parking: "Il y a de nombreuses places de parking disponibles gratuitement sur place.",
       taxis:
         "<1>Rookery Hall est située en zone rurale, nous vous conseillons de réserver vos taxis à l'avance via <2>A Star Taxis</2> (<3>+44 1270 895 044</3>).</1>",
       title: "Transport",
@@ -85,11 +85,11 @@ export default {
     "3": "et",
     firstPlaceholder: "Prénom",
     lastPlaceholder: "Nom",
-    query: "Nous avons hâtes de fêter cet événement avec vous. Pour commencer, veuillez saisir votre nom tel qu'il appararaît sure votre invitation.",
-    query2: "Si vous répondez pour une famille ou un couple, il vous suffit de saisir l'un de vos nom pour afficher tout le groupe.",
+    query: "Nous avons hâte de fêter cet événement avec vous. Pour commencer, veuillez saisir votre nom tel qu'il apparaît sur votre invitation.",
+    query2: "Si vous répondez pour une famille ou un couple, il vous suffit de saisir l'un de vos noms pour afficher tout le groupe.",
     error: {
       userNotFound: "Nous n'avons trouvé aucun résultat pour ce nom.",
-      userNotFound2: "Nous avons des difficutés à vous trouver dans la liste.",
+      userNotFound2: "Nous avons des difficultés à vous trouver dans la liste.",
       noUserHelp:
         "Veuillez vérifier que l'orthographe correspond bien à celle de votre invitation. Si le problème persiste, n'hésitez pas a nous contacter directement.",
       noFirstLast: "Veuillez renseigner votre prénom et votre nom de famille.",
@@ -97,49 +97,49 @@ export default {
   },
   isAttending: {
     children:
-      "Combien d'enfants seront présents? Nous serions ravis de les voir parmi nous, mais si vous préférez profiter de notre mariage pour vous offrir une soirée de repos bien mértée, nous comprendrons tout à fait.",
+      "Combien d'enfants seront présents ? Nous serions ravis de les voir parmi nous, mais si vous préférez profiter de notre mariage pour vous offrir une soirée de repos bien méritée, nous comprendrons tout à fait.",
     dietry:
-      "Merci de nous indiquer si vous êtes végétarien, végétalien (vegan) ou si vous avez des allergies alimentaires ou des besoins diétiques particuliers au sein de votre groupe, afin que nous puissions en informer le chef.",
+      "Merci de nous indiquer si vous êtes végétarien, végétalien (végan) ou si vous avez des allergies alimentaires ou des besoins diététiques particuliers au sein de votre groupe, afin que nous puissions en informer le chef.",
     notes:
-      "Souhaitez-vous nous faire part d'autres chose? N'hesitez pas a nous transmettre toute information ou demande particuliere ici - qu'il s'agisse de besoin en matiêre d'accessibilité, de questions sur transport ou de toute autre élélement pouvant rendre votre journé plus agréable.",
+      "Souhaitez-vous nous faire part d'autres choses ? N'hésitez pas à nous transmettre toute information ou demande particulière ici - qu'il s'agisse de besoin en matière d'accessibilité, de questions sur le transport ou de tout autre élément pouvant rendre votre journée plus agréable.",
   },
   isNotAttending: {
     text1: "Vous allez nous manquer !",
     text2:
       "Merci de nous avoir prévenus. Nous sommes désolés que vous ne puissiez pas venir, mais nous comprenons tout à fait.",
     text3:
-      "N'hesitez pas à parcourir notre site si vous souhaiteez consulter les détails de notre jour. Nous ne manquerons pas de partager les photos avec vous après le grand jour !",
+      "N'hésitez pas à parcourir notre site si vous souhaitez consulter les détails de la journée. Nous ne manquerons pas de partager les photos avec vous après le grand jour !",
     text4:
-      "Si vous souhaitez nous laisser un message ou vos voeux de boheur, n'hesitez pas à les partager ici :",
+      "Si vous souhaitez nous laisser un message ou vos vœux de bonheur, n'hésitez pas à les partager ici :",
   },
   form: {
     submitSuccessHeader: "Merci pour votre réponse",
     submitSuccessBody:
-      "Si vous avez besoin de modifier quoi que ce soit avant le 1er juillet, il vous suffit de revenir sur le site pour metre à jour votre formulaire. ",
-    submitSuccessBody2: "N'hesitez pas à revenir quand vous le souhaite pour consulter les détails de la journée.",
+      "Si vous avez besoin de modifier quoi que ce soit avant le 1er juillet, il vous suffit de revenir sur le site pour mettre à jour votre formulaire. ",
+    submitSuccessBody2: "N'hésitez pas à revenir quand vous le souhaitez pour consulter les détails de la journée.",
     submitSuccessAttending: "Nous avons hâte de vous retrouver le 22 août !",
     submitSuccessNotAttending1:
-      "Si vous changez d'avis avant le 1er juillet, il vous suffit de revenir sure ce site pour mettre à jour votre réponse.",
+      "Si vous changez d'avis avant le 1er juillet, il vous suffit de revenir sur ce site pour mettre à jour votre réponse.",
     submitSuccessNotAttending2: "Avec toute notre affection, Sam et Claudine",
     submitFailed: "Échec de l'envoi de votre RSVP.",
     title: "Confirmation de présence",
     respondBy: "Merci de nous confirmer votre présence avant le",
-    respondByDate: "1er July 2026",
+    respondByDate: "1er juillet 2026",
     description:
-      "<1>Bienvenue {{- names}}. Nous sommes tellement excités de partager cette journée avec vous. Si ce n'est pas vous, merci de <2>cliquez ici pour changer d'uilisateur</2></1>.",
+      "<1>Bienvenue {{- names}}. Nous sommes tellement excités de partager cette journée avec vous. Si ce n'est pas vous, merci de <2>cliquer ici pour changer d'utilisateur</2></1>.",
     descriptionReceived:
-      "<1>Merci {{- names}}. Nous avons déjà reçu votre réponse. Si ce n'est pas vous, merci de <2>cliquez ici pour changer d'utilisateur</2></1>",
+      "<1>Merci {{- names}}. Nous avons déjà reçu votre réponse. Si ce n'est pas vous, merci de <2>cliquer ici pour changer d'utilisateur</2></1>",
     editButton: "Modifier ma réponse",
     submit: "Envoyer ma réponse",
   },
   mealSelector: {
-    title: "Le menu. Veuillez selectioner vos {{- partner_possessive}} choix pour le dîner de mariage ci-dessous :",
+    title: "Le menu. Veuillez sélectionner les choix {{- partner_possessive}} pour le dîner de mariage ci-dessous :",
     starter: "Entrée",
     main: "Plat Principal",
     dessert: "Dessert",
     partner: " Partenaire",// keep the space at the front
     partner_possessive: " de votre partenaire", // keep the space
-    placeholder: "Selectionnez votre {{type}}",
-    placeholder_partner: "Selectionnez {{type}} du votre partenaire"
+    placeholder: "Sélectionnez votre {{type}}",
+    placeholder_partner: "Sélectionnez {{type}} de votre partenaire"
   },
 } as const;
